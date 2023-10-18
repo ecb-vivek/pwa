e6bb9376b6c66165bead30ca10f69f1efbe15e25
ecb-vivek <147471094+ecb-vivek@users.noreply.github.com>
2023-10-18 17:34:53 +0530

    1

---
+++ src/script.js
@@ -0,0 +1,9 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("serviceWorker.js").then(registration => {
+        console.log("service worker Register.");
+        console.log(registration);
+    }).catch(err => {
+        console.log("service worker failed");
+        console.log(err);
+    });
+}
