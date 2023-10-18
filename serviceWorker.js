3a037c85fb4001fd48b16a8246db35d129513446
ecb-vivek <147471094+ecb-vivek@users.noreply.github.com>
2023-10-18 17:51:10 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,9 +1,11 @@
 self.addEventListener("install", e => {
-    e.waitUntil(
+
+    e.waitUntil(self.skipWaiting());
+   /* e.waitUntil(
         caches.open("static").then(cache => {
             return cache.addAll(["./", "./src/style.css", "./images/first.jpg"])
         })
-    );
+    ); */
 });
 
 self.addEventListener("fetch", e => {
