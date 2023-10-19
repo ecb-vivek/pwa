4799df6305b067f211b08291337a2b943381d3f2
ecb-vivek <147471094+ecb-vivek@users.noreply.github.com>
2023-10-19 17:09:28 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -9,7 +9,6 @@ if ("serviceWorker" in navigator) {
 const button = document.querySelector("button");
 
 button.addEventListener("click", () => {
-    debugger;
     Notification.requestPermission().then(res => {
         if (res === "granted") {
             const notify = new Notification("pwa Notification", {
