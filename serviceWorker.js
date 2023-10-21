9cd4557e0bc2b11c578ee0c2eb658809b0c6880f
ecb-vivek <vivek@easycloudbooks.com>
2023-10-21 16:30:05 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -9,8 +9,13 @@ self.addEventListener("install", e => {
 
 self.addEventListener('push', e => {
     const data = e.data.json();
-    self.registration.showNotification(data.title, {
-        body: data.body,
+    self.registration.showNotification("PWA Notification", {
+        body: {
+            body: "Hello Notification",
+            icon: "images/second.png",
+            badge:"images/second.png",
+            tag: "first"
+        },
         icon: 'images/second.png',
         tag: 'push-notification'
     })
